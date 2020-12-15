ad1cb32c2628020e9aa9339bee5e63674ab93a1a
Subhadip Dutta <dsubhadip21@gmail.com>
2020-12-15 21:06:18 +0530

    Configured docker with node js and removed .env files

--- pern-todo-backend/db.js
+++ pern-todo-backend/db.js
@@ -11,26 +11,18 @@ let localDB = {
 }
 
 
-// user: 'yetziszemhkxri',
-// password: 'c794979d4e3686e9064413481f3e8b2a0cf67054f606fecbea29ad9e14706371',
-// host: 'ec2-3-231-48-230.compute-1.amazonaws.com',
-// port: 5432,
-// database: 'd36hn1jthlufjl'
-
-// let stageDB = {
-//     user: 'yetziszemhkxri',
-//     password: 'c794979d4e3686e9064413481f3e8b2a0cf67054f606fecbea29ad9e14706371',
-//     host: 'ec2-3-231-48-230.compute-1.amazonaws.com',
-//     port: 5432,
-//     database: 'd36hn1jthlufjl'
-// }
-
 // const pool = new Pool(localDB)
+
+// heroku
+// const pool = new Client({
+//     connectionString: process.env.POSTGRES_URI,
+//     ssl: {
+//         rejectUnauthorized: false
+//     }
+// });
+
 const pool = new Client({
-    connectionString: process.env.POSTGRES_URI,
-    ssl: {
-        rejectUnauthorized: false
-    }
+    connectionString: 'postgres://dockerpg:dockerpg@pg:5432/dockerpg',    
 });
 
 
